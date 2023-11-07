3234f7d8da7387e9c84fe135ed24fd8d90dc4993
NerKhoza <khoza.ner.142@gmail.com>
2023-11-07 14:59:07 -0800

    update

---
+++ Betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function It prints "Hello, Betty!" 
+ * to the standard output
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	printf("Hello, Betty!\n");
+
+	return (0);
+}
