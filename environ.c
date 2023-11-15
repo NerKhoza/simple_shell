bf93979a1834e8120e63600fe049fdb8acf1a383
thulasizwe33 <thulasizwe3@gmail.com>
2023-11-15 08:48:25 -0800

    environ.c

--- environ.c
+++ environ.c
@@ -2,8 +2,8 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing the potential arguments.
+ * Used to maintain the constant function prototype.
  * Return: Always 0
  */
 int _myenv(info_t *info)
@@ -36,10 +36,10 @@ char *_getenv(info_t *info, const char *name)
 
 /**
  * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * or modify an existing one
+ * @info: Structure containing potential arguments.
+ * Used to maintain constant function prototype.
+ * Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
@@ -55,9 +55,9 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * @info: Structure containing potential arguments.
+ * Used to maintain constant function prototype.
+ * Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
@@ -76,8 +76,8 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing potential arguments.
+ * Used to maintain constant function prototype.
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
