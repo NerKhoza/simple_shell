c71253cab32daf99ec07b7ebd18d8242ad626d1b
NerKhoza <khoza.ner.142@gmail.com>
2023-11-17 04:43:01 -0800

    update

--- errors.c
+++ errors.c
@@ -1,7 +1,6 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
  *_eputs - a function prints an input string
  * @str: function parameter
  */
@@ -15,38 +14,13 @@ void _eputs(char *str)
 	{
 		_eputchar(str[x]);
 		x++;
-=======
- *_eputs - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
- */
-void _eputs(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
->>>>>>> origin/master
 	}
 }
 
 /**
-<<<<<<< HEAD
  * _eputchar - a function that writes the character c to stderr
  * @c : function parameter
  * Return: 1, -1 and errno
-=======
- * _eputchar - writes the character c to the stderr
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
->>>>>>> origin/master
  */
 int _eputchar(char c)
 {
@@ -64,7 +38,6 @@ int _eputchar(char c)
 }
 
 /**
-<<<<<<< HEAD
  * _putfd - a function that writes the character c to given fd
  * @c: parameter 1
  * @fd: parameter 2
@@ -82,26 +55,6 @@ int _putfd(char c, int fd)
 	}
 	if (c != BUF_FLUSH)
 		buf[x++] = c;
-=======
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putfd(char c, int fd)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
 	return (1);
 }
 
@@ -187,43 +140,25 @@ int _putfd(char c, int fd)
 	}
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
->>>>>>> origin/master
 	return (1);
 }
 
 /**
-<<<<<<< HEAD
  *_putsfd - a function that prints an input string
  * @str: parameter 1
  * @fd: parameter 2
-=======
- *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
->>>>>>> origin/master
  * Return: the number of chars put
  */
 int _putsfd(char *str, int fd)
 {
-<<<<<<< HEAD
 	int x = 0;
-=======
-	int i = 0;
->>>>>>> origin/master
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-<<<<<<< HEAD
 		x += _putfd(*str++, fd);
 	}
 	return (x);
-=======
-		i += _putfd(*str++, fd);
-	}
-	return (i);
->>>>>>> origin/master
 }
 
