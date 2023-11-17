d748a0631245f8305002cab6cd24baf254b02f58
NerKhoza <khoza.ner.142@gmail.com>
2023-11-17 07:34:45 -0800

    update

--- main.c
+++ main.c
@@ -6,18 +6,11 @@
  * @ac: parameter 1
  * @av: parameter 2
  * Return: 0 and 1
-=======
- * main - entry point.
- * @ac: arg count.
- * @av: arg vector.
- *
- * Return: 0 on success, 1 on error
->>>>>>> origin/master
  */
+ 
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-<<<<<<< HEAD
 	int bf = 2;
 
 	asm ("mov %1, %0\n\t"
@@ -29,45 +22,24 @@ int main(int ac, char **av)
 	{
 		bf = open(av[1], O_RDONLY);
 		if (bf == -1)
-=======
-	int fd = 2;
-
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
->>>>>>> origin/master
+		if (errno == EACCES)
+			exit(126);
+		if (errno == ENOENT)
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
+			_eputs(av[0]);
+			_eputs(": 0: Can't open ");
+			_eputs(av[1]);
+			_eputchar('\n');
+			_eputchar(BUF_FLUSH);
+			exit(127);
 		}
-<<<<<<< HEAD
+		return (EXIT_FAILURE);
+	}
 		info->readfd = bf;
-=======
-		info->readfd = fd;
->>>>>>> origin/master
 	}
 	populate_env_list(info);
 	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
